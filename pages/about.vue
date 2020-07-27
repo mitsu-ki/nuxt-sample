e3010db13d4c75acdca6b59e92e714a9d35878e6
mitsu-ki <mitsuhiro.kine@4digit.jp>
2020-07-27 15:32:56 +0900

    [add] create-nuxt-app で full static なサンプル環境追加
    https://ja.nuxtjs.org/blog/going-full-static/
    
    [fix] prettier

---
+++ pages/about.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="about">
+        <h1>This is about page</h1>
+        <p>staticフォルダからの画像の読み込みは、このようにします。</p>
+        <figure>
+          <img src="/images/about/nuxt-square.svg" alt="nuxt.js" />
+          <figcaption>Nuxt.js</figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.about h1 {
+  color: gray;
+  font-size: 26px;
+}
+</style>
