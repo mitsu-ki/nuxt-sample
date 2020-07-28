faf2b18dd40b8827e6d8479955510c15979c49ff
mitsu-ki <mitsuhiro.kine@4digit.jp>
2020-07-28 19:35:22 +0900

    [fix] amp validator
    https://validator.ampproject.org/

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
-const modifyHtml = html => {
+const ampBoilerplate =
+  "<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>";
+const modifyHtml = (html, isGenerate) => {
+  html = html.replace(/<html/gi, "<html amp ");
   // Add amp-custom tag to added CSS
   html = html.replace(/<style data-vue-ssr/g, "<style amp-custom data-vue-ssr");
   // Remove every script tag from generated HTML
@@ -6,10 +9,12 @@ const modifyHtml = html => {
     /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
     ""
   );
+  // Replace img tag
+  if (isGenerate) html = html.replace(/<img/gi, "<amp-img");
   // Add AMP script before </head>
   const ampScript =
     '<script async src="https://cdn.ampproject.org/v0.js"></script>';
-  html = html.replace("</head>", ampScript + "</head>");
+  html = html.replace("</head>", ampScript + ampBoilerplate + "</head>");
   return html;
 };
 
@@ -68,7 +73,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: [["nuxt-canonical", { baseUrl: "https://my-nuxt-sample.com" }]],
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
@@ -86,17 +91,17 @@ export default {
 
   hooks: {
     "generate:page": page => {
-      page.html = modifyHtml(page.html);
+      page.html = modifyHtml(page.html, true);
 
       // 特定ページをAMP対応する場合
       // if (page.route === '/enterprise/use-case/') {
-      //   page.html = modifyHtml(page.html);
+      //   page.html = modifyHtml(page.html, true);
       // }
     },
     "render:route": (url, page, { req, res }) => {
-      page.html = modifyHtml(page.html);
+      page.html = modifyHtml(page.html, false);
       // if (page.route === '/enterprise/use-case/') {
-      //   page.html = modifyHtml(page.html);
+      //   page.html = modifyHtml(page.html, false);
       // }
     }
   }
--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,12 @@
         <h1>This is about page</h1>
         <p>staticフォルダからの画像の読み込みは、このようにします。</p>
         <figure>
-          <img src="/images/about/nuxt-square.svg" alt="nuxt.js" />
+          <img
+            src="/images/about/nuxt-square.svg"
+            alt="nuxt.js"
+            width="430"
+            height="430"
+          />
           <figcaption>Nuxt.js</figcaption>
         </figure>
       </div>
